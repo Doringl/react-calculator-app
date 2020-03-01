428873edc8f99508b74bb264cbc06fb2bef22e2d
Doringl <49004028+Doringl@users.noreply.github.com>
2020-03-01 20:07:55 +0300

    Updates

---
+++ precache-manifest.a041f0551012731c91175ee90370120a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "14a71ef20d5aca8e90b0186e7b42baf5",
+    "url": "/react-calculator-app/index.html"
+  },
+  {
+    "revision": "3a48009a9356383b8e6c",
+    "url": "/react-calculator-app/static/css/2.777f1ffd.chunk.css"
+  },
+  {
+    "revision": "7cca00b16b72940999ee",
+    "url": "/react-calculator-app/static/css/main.99194505.chunk.css"
+  },
+  {
+    "revision": "3a48009a9356383b8e6c",
+    "url": "/react-calculator-app/static/js/2.d2fd61b1.chunk.js"
+  },
+  {
+    "revision": "a1eb64b2915c087f20f6749be2413a7f",
+    "url": "/react-calculator-app/static/js/2.d2fd61b1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7cca00b16b72940999ee",
+    "url": "/react-calculator-app/static/js/main.ff32eefa.chunk.js"
+  },
+  {
+    "revision": "1cce7ff9c3a2f46ec8ef",
+    "url": "/react-calculator-app/static/js/runtime-main.a4aa17cb.js"
+  }
+]);
